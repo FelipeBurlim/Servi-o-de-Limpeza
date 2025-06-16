9e2d0e16886049ee48cc00b899631c1d4cc412f6
Felipe Burlim Favaretto <feeleburlim31@gmail.com>
2025-06-16 10:51:38 -0300

    Add submenu clientes moita,arq txt e algumas correções

--- ServiçoLimpeza.py
+++ ServiçoLimpeza.py
@@ -1,5 +1,3 @@
-#duvida: setup dos dados no listar boa ou não (listars) /// incluir apenas 1 (incluir) /// mais de um telefone em faxineiro ///conferir cada dado incluindo (incluir) /// perguntar o dado pa alterar e encher de if ou ter que reinserir todos os dados (alterar)
-
 #faxineiros -eu
 def submenu_faxineiros():
     print("Submenu de faxineiros:")
@@ -12,49 +10,66 @@ def submenu_faxineiros():
     opção=int(input())
     faxineiros(opção)
 
+def carregar_dados_faxineiros(nome_arq):
+    FaxineirosDic = {}
+    if existe_arq(nome_arq):
+        arq=open(nome_arq, "r")
+        for linha in arq:
+            linha = linha.replace("\n", " ")
+            linha = linha.split(";")
+            FaxineirosDic[linha[0]]=[]
+            FaxineirosDic[linha[0]].append(linha[1])
+            FaxineirosDic[linha[0]].append(linha[2])
+            FaxineirosDic[linha[0]].append(linha[3])
+            FaxineirosDic[linha[0]].append(linha[4])
+            FaxineirosDic[linha[0]].append(linha[5])
+    else:
+            print("Arquivo não encontrado! ")
+    arq.close()
+    return FaxineirosDic
+
 def faxineiros(op):
+    nome_arq="./faxineiros.txt"
+    FaxineirosDic=carregar_dados_faxineiros(nome_arq)
     if op==1:
-        listartd_f()
+        listartd_f(FaxineirosDic)
     elif op==2:
         print("Cpf do faxineiro:")
         cpf=input()
-        listar_f(cpf)
+        listar_f(FaxineirosDic,cpf)
     elif op==3:
-        incluir_f()
+        incluir_f(FaxineirosDic)
     elif op==4:
         print("Cpf do faxineiro: ")
         cpf=input()
-        alterar_f(cpf)
+        alterar_f(FaxineirosDic,cpf)
     elif op==5:
         print("Cpf do faxineiro: ")
         cpf=input()
-        excluir_f(cpf)
+        excluir_f(FaxineirosDic,cpf)
     else:
         print("Digite uma opção válida")
 
-def listartd_f():
-    FaxineirosDic={'42565796901':['Victor Hugo Zivas','Homem',3393984,'16/09/2006',1499093210],'67765732652':['Leonardo Almeida Quente','Homem', 1243753,'10/02/2007',14996627901],'34155646432':['Pedro Boschetti Chongas','Mulher',2136778, '14/12/2006',14996001179],'65266576523':['Teo Brazola Tolis','Homem',7643423,'23/09/2006',14995632020],'36786543701':['Pedro Peres Penis','Mulher',3231000,'21/04/2007',19059237312]}    
+def listartd_f(dic):
     print("Faxineiros:")
-    for i in FaxineirosDic:
-        print(f"\t {FaxineirosDic[i][0]}:")
-        for j in range(1,len(FaxineirosDic[i])):
-            print(f"\t\t {FaxineirosDic[i][j]}")
+    for i in dic:
+        print(f"\t {dic[i][0]}:")
+        for j in range(1,len(dic[i])):
+            print(f"\t\t {dic[i][j]}")
         print()
         
-def listar_f(cpf):
-    FaxineirosDic={'42565796901':['Victor Hugo Zivas','Homem',3393984,'16/09/2006',1499093210],'67765732652':['Leonardo Almeida Quente','Homem', 1243753,'10/02/2007',14996627901],'34155646432':['Pedro Boschetti Chongas','Mulher',2136778, '14/12/2006',14996001179],'65266576523':['Teo Brazola Tolis','Homem',7643423,'23/09/2006',14995632020],'36786543701':['Pedro Peres Penis','Mulher',3231000,'21/04/2007',19059237312]}    
-    if cpf in FaxineirosDic:
-        print(FaxineirosDic[cpf][0],":")
-        for i in range(1,len(FaxineirosDic[cpf])):
-            print(f"\t {FaxineirosDic[cpf][i]}")
+def listar_f(dic,cpf):
+    if cpf in dic:
+        print(dic[cpf][0],":")
+        for i in range(1,len(dic[cpf])):
+            print(f"\t {dic[cpf][i]}")
     else:
         print("Cpf inválido: faxineiro inexistente ou cpf incorreto.")
     
-def incluir_f():
-    FaxineirosDic={'42565796901':['Victor Hugo Zivas','Homem',3393984,'16/09/2006',1499093210],'67765732652':['Leonardo Almeida Quente','Homem', 1243753,'10/02/2007',14996627901],'34155646432':['Pedro Boschetti Chongas','Mulher',2136778, '14/12/2006',14996001179],'65266576523':['Teo Brazola Tolis','Homem',7643423,'23/09/2006',14995632020],'36786543701':['Pedro Peres Penis','Mulher',3231000,'21/04/2007',19059237312]}    
+def incluir_f(dic):
     print("Entre com os dados do faxineiro: ")
     cpf=input("Cpf: ")
-    if cpf in FaxineirosDic:
+    if cpf in dic:
         print("Cpf ja cadastrado.")
     else:
         nome=input("Nome: ")
@@ -62,35 +77,53 @@ def incluir_f():
         rg=int(input("RG: "))
         data=input("Data de nascimento: ")
         num=int(input("Numero de contato: "))
-        FaxineirosDic[cpf]=[nome,sexo,rg,data,num]
-        if cpf in FaxineirosDic:
+        dic[cpf]=[nome,sexo,rg,data,num]
+        if cpf in dic:
             print("Faxineiro incluído com sucesso.")
         else:
             print("Erro no cadastro do faxineiro.")
 
-def alterar_f(cpf):
-    FaxineirosDic={'42565796901':['Victor Hugo Zivas','Homem',3393984,'16/09/2006',1499093210],'67765732652':['Leonardo Almeida Quente','Homem', 1243753,'10/02/2007',14996627901],'34155646432':['Pedro Boschetti Chongas','Mulher',2136778, '14/12/2006',14996001179],'65266576523':['Teo Brazola Tolis','Homem',7643423,'23/09/2006',14995632020],'36786543701':['Pedro Peres Penis','Mulher',3231000,'21/04/2007',19059237312]}    
-    if cpf in FaxineirosDic:
+def alterar_f(dic,cpf):
+    if cpf in dic:
         print("Insira os dados novos: ")
         nome=input("Nome: ")
         sexo=input("Sexo: ")
         rg=int(input("RG: "))
         data=input("Data de nascimento: ")
         num=int(input("Numero de contato: "))
-        FaxineirosDic[cpf]=[nome,sexo,rg,data,num]
+        dic[cpf]=[nome,sexo,rg,data,num]
         print("Alteração concluída.")
     else:
         print("Cpf não encontrado.")
 
-def excluir_f(cpf):
-    FaxineirosDic={'42565796901':['Victor Hugo Zivas','Homem',3393984,'16/09/2006',1499093210],'67765732652':['Leonardo Almeida Quente','Homem', 1243753,'10/02/2007',14996627901],'34155646432':['Pedro Boschetti Chongas','Mulher',2136778, '14/12/2006',14996001179],'65266576523':['Teo Brazola Tolis','Homem',7643423,'23/09/2006',14995632020],'36786543701':['Pedro Peres Penis','Mulher',3231000,'21/04/2007',19059237312]}    
-    if cpf in FaxineirosDic:
-        del FaxineirosDic[cpf]
+def excluir_f(dic,cpf):
+    if cpf in dic:
+        del dic[cpf]
         print("Remoção concluída.")
     else:
         print("Cpf não encontrado.")
 
 #cliente -moita
+def carregar_dados_clientes(nome_arq):
+    dicio_clientes = {}
+    if existe_arq(nome_arq):
+        arq=open(nome_arq, "r")
+        for linha in arq:
+            linha = linha.replace("\n", " ")
+            linha = linha.split(";")
+            dicio_clientes[linha[0]]=[]
+            dicio_clientes[linha[0]].append(linha[1])
+            dicio_clientes[linha[0]].append(linha[2])
+            dicio_clientes[linha[0]].append(linha[3])
+            dicio_clientes[linha[0]].append(linha[4])
+            dicio_clientes[linha[0]].append(linha[5])
+            dicio_clientes[linha[0]].append(linha[6])
+            dicio_clientes[linha[0]].append(linha[7])
+    else:
+            print("Arquivo não encontrado! ")
+    arq.close()
+    return dicio_clientes
+
 def submenu_clientes():
     print("Submenu de clientes:")
     print("1. Listar todos clientes")
@@ -98,11 +131,92 @@ def submenu_clientes():
     print("3. Incluir cliente")
     print("4. Alterar clientes")
     print("5. Excluir cliente")
-    opção=input()
-    clientes(opção)
+    print("Escolha uma ação:")
+    opção=int(input())
+    return opção
 
-def clientes(op):
-    #igualmain
+def clientes():
+    nome_arq = "./clientes.txt"
+    clientes= carregar_dados_clientes(nome_arq)
+    op=submenu_clientes()
+    if op ==1:
+        listar_clientes(clientes)
+    elif op ==2:
+        cpf=input("Insira o CPF do cliente : ")
+        listar_1cliente(cpf,clientes)
+        if listar_1cliente:
+            print("Aqui estão os dados do cliente!")
+        else:
+            print("CPF não cadastrado!")
+    elif op ==3:
+        print("Cadastrando novo cliente... ")
+        cpf = input(" Inisira seu CPF: ")
+        add_cliente(cpf,clientes)
+    elif op==4:
+        cpf = input(" Inisira o CPF do cliente que deseja alterar os dados : ")
+        alterar_cliente(cpf,clientes)
+    elif op == 5:
+        cpf = input(" Inisira o CPF do cliente que deseja excluir : ")
+        excl_cliente(cpf,clientes)
+        
+def listar_clientes(ClientesDic):
+    for cpf, info in ClientesDic.items():
+        print(f"{cpf}")
+        for j in info:
+            print(j)
+        print("----------------------------------------------------------")
+
+def listar_1cliente(cpf,clientes):
+    ClientesDic= clientes
+    if cpf in ClientesDic:
+        for i in range(len(ClientesDic[cpf])):
+            print(ClientesDic[cpf][i])
+        return True
+    else:
+        return False
+    
+def add_cliente(cpf,dicio):
+    ClientesDic= dicio
+    if cpf in ClientesDic:
+        print("Cpf ja cadastrado.")
+    else:
+        nome=input("Insira seu nome : ")
+        dn=input("Insira sua data de nascimento : ")
+        end=input("Insira seu endereço : ")
+        cep=input("Insira seu CEP : ")
+        cidade=input("Insira sua cidade : ")
+        email=input("Insira seu e-mail : ")
+        tel=input("Insira seu telefone : ")
+        ClientesDic[cpf]=[nome,dn,end,cidade,email,tel]
+        grava_dados(ClientesDic, "./clientes.txt")
+
+def alterar_cliente(cpf,dicio):
+    ClientesDic= dicio
+    if cpf in ClientesDic:
+        print("Insira os novos dados do cliente : ")
+        nome=input("Insira seu nome : ")
+        dn=input("Insira sua data de nascimento : ")
+        end=input("Insira seu endereço : ")
+        cep=input("Insira seu CEP : ")
+        cidade=input("Insira sua cidade : ")
+        email=input("Insira seu e-mail : ")
+        tel=input("Insira seu telefone : ")
+        ClientesDic[cpf]=[nome,dn,end,cep,cidade,email,tel]
+        grava_dados(ClientesDic, "./clientes.txt")
+        print("Alteração concluida!")
+        
+    else:
+        print("CPF não registrado!")
+
+def excl_cliente(cpf,dicio):
+    ClientesDic= dicio
+    if cpf in ClientesDic:
+        del ClientesDic[cpf]
+        grava_dados(ClientesDic, "./clientes.txt")
+        print(ClientesDic)
+        print("Remoção concluida com sucesso!")
+    else:
+        print("CPF não registrado!")  
 
 #serviços -moita
 def submenu_serviços():
@@ -116,11 +230,29 @@ def submenu_serviços():
     serviços(opção)
 
 def serviços(op):
-    #igualmain
+    print()
 
 #relatorios - eu
 
 #main/menu
+
+def existe_arq(nome_arq):
+    import os
+    if os.path.exists(nome_arq):
+        return True
+    else:
+        return False
+    
+
+def grava_dados(dicio,nome_arq):
+    arq = open(nome_arq, "w")
+    for cpf in dicio:
+        arq.write(cpf + ";")
+        for i in range(len(dicio[cpf])):
+            arq.write(dicio[cpf][i]+ ";")
+        arq.write("\n")
+    arq.close()
+    
 def menu():
     print("Menu Principal:")
     print("1. Submenu de Faxineiros")
@@ -139,11 +271,14 @@ def main():
         if op==1:
             submenu_faxineiros()
         elif op==2:
-            submenu_clientes()
+            clientes()
         elif op==3:
             submenu_serviços()        
         elif op==4:
-            #relatorios
+            print()
+            # submenu_relatórios()
+        elif op==5:
+            print("Encerrando programa...")
         else:
             print("Digite uma opção válida!")
 
